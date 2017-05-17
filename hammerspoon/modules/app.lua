f50b3d5e44d2f3714c9465399aca38a8bcd8be54
김정환 <ej88ej@gmail.com>
2017-05-17 23:50:20 +0900

    hammerspoon: App focus

---
+++ hammerspoon/modules/app.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local function bindApp(mods)
+  return function(key, app)
+    hs.hotkey.bind(mods, key, function()
+      hs.application.launchOrFocus(app)
+    end)
+  end
+end
+
+function M.init(mods)
+  local bindAppwithMods = bindApp(mods)
+
+  bindAppwithMods('a', 'atom')
+  bindAppwithMods('c', 'Google Chrome')
+  bindAppwithMods('e', 'Evernote')
+  bindAppwithMods('i', 'IntelliJ IDEA')
+  bindAppwithMods('m', 'Mail')
+  bindAppwithMods('n', 'Notes')
+  bindAppwithMods('t', 'iTerm')
+end
+
+return M
---
+++ hammerspoon/modules/vim.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+local function bindApp(mods)
+  return function(key, modifiers, character)
+    hs.hotkey.bind(mods, key, function()
+      hs.eventtap.keyStroke(modifiers, character, 1)
+    end)
+  end
+end
+
+function M.init()
+  local bindAppWithCtrl = bindApp({'ctrl'})
+  bindAppWithCtrl('h', {}, 'left')
+  bindAppWithCtrl('j', {}, 'down')
+  bindAppWithCtrl('k', {}, 'up')
+  bindAppWithCtrl('l', {}, 'right')
+
+  local bindAppWithCtrlShift = bindApp({'ctrl', 'shift'})
+  bindAppWithCtrlShift('h', {'shift'}, 'left')
+  bindAppWithCtrlShift('l', {'shift'}, 'right')
+  bindAppWithCtrlShift('k', {'shift'}, 'up')
+  bindAppWithCtrlShift('j', {'shift'}, 'down')
+
+  local bindAppWithCtrlAlt = bindApp({'ctrl', 'alt'})
+  bindAppWithCtrlAlt('h', {'alt'}, 'left')
+  bindAppWithCtrlAlt('l', {'alt'}, 'right')
+
+  local bindAppWithCtrlAltShift = bindApp({'ctrl', 'alt', 'shift'})
+  bindAppWithCtrlAltShift('h', {'alt', 'shift'}, 'left')
+  bindAppWithCtrlAltShift('l', {'alt', 'shift'}, 'right')
+
+  local bindAppWithCtrlCmd = bindApp({'ctrl', 'cmd'})
+  bindAppWithCtrlCmd('h', {'cmd'}, 'left')
+  bindAppWithCtrlCmd('l', {'cmd'}, 'right')
+  bindAppWithCtrlCmd('k', {'cmd'}, 'up')
+  bindAppWithCtrlCmd('j', {'cmd'}, 'down')
+
+  local bindAppWithCtrlCmdShift = bindApp({'ctrl', 'cmd', 'shift'})
+  bindAppWithCtrlCmdShift('h', {'cmd', 'shift'}, 'left')
+  bindAppWithCtrlCmdShift('l', {'cmd', 'shift'}, 'right')
+  bindAppWithCtrlCmdShift('k', {'cmd', 'shift'}, 'up')
+  bindAppWithCtrlCmdShift('j', {'cmd', 'shift'}, 'down')
+end
+
+return M
