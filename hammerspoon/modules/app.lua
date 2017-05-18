a08d88cf7409ce795a2ebe10adc03e19a16f6e47
김정환 <ej88ej@gmail.com>
2017-05-18 14:03:07 +0900

    hammerspoon: update j, i

--- hammerspoon/modules/app.lua
+++ hammerspoon/modules/app.lua
@@ -11,15 +11,14 @@ end
 function M.init(mods)
   local bindAppwithMods = bindApp(mods)
 
-  bindAppwithMods('a', 'atom')
-  bindAppwithMods('c', 'Google Chrome')
   bindAppwithMods('e', 'Evernote')
-  bindAppwithMods('i', 'IntelliJ IDEA')
+  bindAppwithMods('h', 'Google Chrome')
+  bindAppwithMods('i', 'iTerm')
+  bindAppwithMods('j', 'IntelliJ IDEA')
   bindAppwithMods('l', 'Line')
   bindAppwithMods('k', 'KakaoTalk')
   bindAppwithMods('m', 'Mail')
   bindAppwithMods('n', 'Notes')
-  bindAppwithMods('t', 'iTerm')
 end
 
 return M
